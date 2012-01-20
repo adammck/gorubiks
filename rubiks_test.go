a3946cd407e310942d746c6edd49bab2aa4e7ae4
Adam Mckaig <adam.mckaig@gmail.com>
2012-01-20 14:26:44 -0500

    Add more piece tests.
    These are way too verbose and repetitive. But I have no idea what I'm
    doing, so I'm being extra careful. I might delete most of these later.

--- rubiks_test.go
+++ rubiks_test.go
@@ -34,3 +34,43 @@ func TestRotateTopAnticlockwise(t *testing.T) {
   if piece[left]   != yellow { t.Error("LEFT should be YELLOW.")  }
   if piece[right]  != blue   { t.Error("RIGHT should be BLUE.")   }
 }
+
+func TestRotateFrontClockwise(t *testing.T) {
+  piece := test_piece.rotate(front, clockwise)
+  if piece[top]    != orange { t.Error("TOP should be ORANGE.")   }
+  if piece[bottom] != white  { t.Error("BOTTOM should be WHITE.") }
+  if piece[front]  != blue   { t.Error("FRONT should be BLUE.")   }
+  if piece[back]   != yellow { t.Error("BACK should be YELLOW.")  }
+  if piece[left]   != green  { t.Error("LEFT should be GREEN.")   }
+  if piece[right]  != red    { t.Error("RIGHT should be RED.")    }
+}
+
+func TestRotateFrontAnticlockwise(t *testing.T) {
+  piece := test_piece.rotate(front, anticlockwise)
+  if piece[top]    != white  { t.Error("TOP should be WHITE.")     }
+  if piece[bottom] != orange { t.Error("BOTTOM should be ORANGE.") }
+  if piece[front]  != blue   { t.Error("FRONT should be BLUE.")    }
+  if piece[back]   != yellow { t.Error("BACK should be YELLOW.")   }
+  if piece[left]   != red    { t.Error("LEFT should be RED.")      }
+  if piece[right]  != green  { t.Error("RIGHT should be GREEN.")   }
+}
+
+func TestRotateLeftClockwise(t *testing.T) {
+  piece := test_piece.rotate(left, clockwise)
+  if piece[top]    != yellow { t.Error("TOP should be YELLOW.")   }
+  if piece[bottom] != blue   { t.Error("BOTTOM should be BLUE.")  }
+  if piece[front]  != red    { t.Error("FRONT should be RED.")    }
+  if piece[back]   != green  { t.Error("BACK should be GREEN.")   }
+  if piece[left]   != orange { t.Error("LEFT should be ORANGE.")  }
+  if piece[right]  != white  { t.Error("RIGHT should be WHITE.")  }
+}
+
+func TestRotateLeftAnticlockwise(t *testing.T) {
+  piece := test_piece.rotate(left, anticlockwise)
+  if piece[top]    != blue   { t.Error("TOP should be BLUE.")      }
+  if piece[bottom] != yellow { t.Error("BOTTOM should be YELLOW.") }
+  if piece[front]  != green  { t.Error("FRONT should be GREEN.")   }
+  if piece[back]   != red    { t.Error("BACK should be RED.")      }
+  if piece[left]   != orange { t.Error("LEFT should be ORANGE.")   }
+  if piece[right]  != white  { t.Error("RIGHT should be WHITE.")   }
+}
